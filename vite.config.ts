3639f809e40e8982e4a7968867c710d8ee9f9c75
Louay ElGarhy <louayelgarhy@gmail.com>
2025-08-12 04:12:38 +0300

    Update site content

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/", // ✅ Important for GitHub Pages (personal site)
+
   server: {
     host: "::",
     port: 8080,
